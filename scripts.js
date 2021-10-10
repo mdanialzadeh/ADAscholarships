0f17184724ab17e2f3d4bce320757855611956f3
mdanialzadeh <mdanialzadeh10@gmail.com>
2021-10-10 18:43:18 -0400

    initial commit

---
+++ scripts.js
@@ -0,0 +1,26 @@
+const About = document.querySelector(".about")
+const NFTs = document.querySelector(".NFT_page")
+const NFTwhales = document.querySelector(".NFT_Whales")
+
+
+const navButtons = document.querySelectorAll("button")
+
+
+
+function buttons () {
+    
+    navButtons[0].addEventListener('click', () => {
+        About.scrollIntoView({behavior: "smooth"})
+})
+
+navButtons[2].addEventListener('click', () => {
+    NFTs.scrollIntoView({behavior: "smooth"})
+})
+
+navButtons[3].addEventListener('click', () => {
+    NFTwhales.scrollIntoView({behavior: "smooth"})
+})
+
+}
+
+buttons ()
